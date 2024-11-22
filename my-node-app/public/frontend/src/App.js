6308019f384db5003089475043bee55e4757834b
sudo-omar <omarabouharb2805@gmail.com>
2024-11-21 17:58:28 -0800

    eliminate questinoarie attmept

--- my-node-app/public/frontend/src/App.js
+++ my-node-app/public/frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 import Dashboard from './Dashboard';
-import Questionaire from './Questionaire';
 import Home from './Home';
 
 
@@ -13,7 +12,6 @@ const App = () => {
         {/* Define Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/questionaire" element={<Questionaire />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
